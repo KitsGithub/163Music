3f60c3085bc206bc06920e218a9340a4913a7842
Mr_kit <mr_kit@Mr-kitdeMacBook-Pro.local>
2018-11-07 17:14:32 +0800

    歌单页面对接接口和逻辑

--- src/renderer/components/songList.vue
+++ src/renderer/components/songList.vue
@@ -1,24 +1,31 @@
 <template>
     <!-- 歌单 -->
-    <div class="subWindow backgroundColor">
+    <div class="subWindow backgroundColor song">
         <el-popover
             placement="bottom-start"
             width="649"
-            trigger="click">
-            <div class="popoverView">
-                <div class="popoverTop">
-                    <el-button class="listButton">全部风格</el-button>
+            trigger="click"
+            v-model="popoverVisible"
+            >
+            <div class="popoverView backgroundColor">
+                <div class="popoverTop" :v-show="all">
+                    <el-button class="listButton">{{all.name}}</el-button>
                 </div>
                 <div class="popoverLeft float-left">
-                    <div v-for="(item,index) in songTypeList"  :key="index">
-                        {{item.name}}
+                    <div class="itemView" v-for="(item,index) in categoryList" :key="index">
+                        <div class="item">
+                            <i class="el-icon-delete"></i>
+                            {{item.name}}
+                        </div>
                     </div>
                 </div>
                 <div class="popoverRight float-left">
-
+                    <div class="tpyeView float-left" v-for="(item,index) in typeList" :key="index">
+                        <el-button class="typeButton float-left backgroundColor" @click="closePopover(type.name)" v-for="(type,index2) in typeList[index]" :key="index2" :class="{tagActived: type.name === actived.name}">{{type.name}}</el-button>
+                    </div>
                 </div>
             </div>
-            <el-button class="listButton"  slot="reference">欧美<i class="el-icon-caret-bottom el-icon--right"></i></el-button>
+            <el-button class="listButton"  slot="reference" @click="openPopover()">{{actived.name}}<i class="el-icon-caret-bottom el-icon--right"></i></el-button>
         </el-popover>
         <div class="hotTagView">
             <span>热门标签:</span>
@@ -26,62 +33,116 @@
                 {{item.name}}
             </el-button>
         </div>
+        <div class="songListView lightgreen">
+            <div class="personalItems float-left" v-for="(item,index) in songList" :key="index">
+                <img :src="item.coverImgUrl" alt="">
+                <div class="playCount">
+                    <span>{{item.playCount}}</span>
+                    <img src="../img/Headset.png" alt="">
+                </div>
+                <!-- <div class="cover" @mouseover="mouseover" @mouseout="mouseout"></div> -->
+                <span>{{item.name}}</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import _Api from '../api/api.js'
+import Tools from '../public/tools.js'
+import { debug } from 'util';
+
     export default {
         data() {
             return {
-                hotTagList:[
-                    {
-                        name:"华语",
-                        idx:"",
-                    },
-                    {
-                        name:"流行",
-                        idx:"",
-                    },
-                    {
-                        name:"摇滚",
-                        idx:"",
-                    },
-                    {
-                        name:"民谣",
-                        idx:"",
-                    },
-                    {
-                        name:"电子",
-                        idx:"",
-                    },
-                    {
-                        name:"轻音乐",
-                        idx:"",
-                    },
-                    {
-                        name:"影视原声",
-                        idx:"",
-                    },
-                    {
-                        name:"ACG",
-                        idx:"",
-                    },
-                    {
-                        name:"怀念",
-                        idx:"",
-                    },
-                    {
-                        name:"治愈",
-                        idx:"",
+                popoverVisible:false,   //popover控制位
+
+                hotTagList:[],          //热门标签
+                categoryList:[],        //分类列表
+                typeList:[],            //类别数据源
+                all:NaN,
+                actived:{name:"华语"},  //当前激活tag
+
+                // 歌单数据
+                songList:[],            //歌单列表
+            }
+        },
+        beforeMount() {
+            this.getAllCategory()
+            this.getHotCategory()
+            this.getSongList("华语")
+        },
+        methods: {
+            getHotCategory() {
+                _Api.GET("playlist/hot",{}, data=> {
+                    this.hotTagList = data.tags
+                })
+            },
+            getAllCategory() {
+                _Api.GET("playlist/catlist",{}, data=>{
+                    // this.category = data;
+                    console.log(data.categories);
+
+                    this.all = data.all
+
+                    // 左侧数据源
+                    let array = []
+                    for (const i in data.categories) {
+                        if (data.categories.hasOwnProperty(i)) {
+                            const element = data.categories[i];
+                            array.push({
+                                idx:parseInt(i),
+                                name:element
+                            })
+                        }
                     }
-                ],
-                songTypeList:[
-                    {name:'语种'},
-                    {name:'风格'},
-                    {name:'场景'},
-                    {name:'情感'},
-                    {name:'主题'}
-                ],
+                    this.categoryList = array;
+
+                    // 右侧数据源
+                    let typeList = []
+                    for (let i = 0; i < this.categoryList.length; i++) {
+                        // debugger
+                        const category = this.categoryList[i];
+                        let TypeArray = []
+                        for (let j = 0; j < data.sub.length; j++) {
+                            const sub = data.sub[j];
+                            if (sub.category === category.idx) {
+                                TypeArray.push(sub)
+                            }
+                        }
+                        if (TypeArray.length) {
+                            typeList.push(TypeArray)
+                        }
+                    }
+                    this.typeList = typeList;
+
+
+                })
+            },
+
+            getSongList(cat) {
+                if (!cat.length) {
+                    return
+                }
+                // http://localhost:3000/top/playlist?limit=10&cat=%E5%8D%8E%E8%AF%AD
+                _Api.GET("top/playlist?cat=" + cat,{}, data => {
+                    this.songList = data.playlists
+
+                    for (let i = 0; i < this.songList.length; i++) {
+                        const item = this.songList[i];
+                        item.playCount = Tools.Calculate(item.playCount)
+                    }
+                })
+            },
+
+            // 用户操作
+            openPopover() {
+                this.popoverVisible = true;
+            },
+            closePopover(name) {
+                this.actived.name = name
+                this.getSongList(name)
+                this.popoverVisible = false;
             }
         }
     }
